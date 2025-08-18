c2f22d2a6b9f5e1cf48f781d443b391990e1223b
Manish Jha <mnshjha05@gmail.com>
2025-08-18 16:25:10 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+    </main>
+  );
+}
