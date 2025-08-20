b681b13100c3c5bb3f921bae6b3b7f7fe77e5e47
Manish Jha <mnshjha05@gmail.com>
2025-08-20 08:25:20 +0530

    pixel art setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +8,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col max-h-20 gap-5 bg-sky-950">
+        <header className="bg-sky-700 px-10 py-5 flex gap-10 text-slate-200 text-2xl">
+          <section>
+            <Link
+              className="p-1 focus:outline-1 focus:outline-dashed focus:outline-slate-200"
+              href="/"
+            >
+              CRDT
+            </Link>
+          </section>
+          <aside>
+            <ul className="flex gap-10">
+              <li>
+                <Link
+                  className="p-1 focus:outline-1 focus:outline-dashed focus:outline-slate-200"
+                  href="/pixel-art"
+                >
+                  Collaborative Pixel Art
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  title="Coming up soon"
+                  className="p-1 focus:outline-1 focus:outline-dashed focus:outline-slate-200"
+                >
+                  Collaborative Editor
+                </Link>
+              </li>
+            </ul>
+          </aside>
+        </header>
+        <main className="px-10 text-slate-200 flex min-h-screen min-w-full flex-col gap-10">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
